5709fd11270804ebb4780a4353e3a533b2d8f3f5
Captain Throwback <captainthrowback@hotmail.com>
2022-05-29 19:19:33 -0400

    prebuilt: add arm binaries and separate common scripts

--- Android.mk
+++ Android.mk
@@ -18,7 +18,6 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(TW_EXCLUDE_PYTHON),true)
     # Dummy file to copy prebuilt python files
     include $(CLEAR_VARS)
-
     LOCAL_MODULE := python3_bin
     LOCAL_MODULE_TAGS := optional
     LOCAL_MODULE_CLASS := EXECUTABLES
@@ -26,11 +25,18 @@ ifneq ($(TW_EXCLUDE_PYTHON),true)
 
     LOCAL_POST_INSTALL_CMD += \
         mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/system/bin; \
-        cp -f $(LOCAL_PATH)/prebuilt/bin/* $(TARGET_RECOVERY_ROOT_OUT)/system/bin/;
+        cp -rf $(LOCAL_PATH)/prebuilt/common/bin $(TARGET_RECOVERY_ROOT_OUT)/system/;
+
+    ifeq ($(TARGET_ARCH),arm64)
+        LOCAL_POST_INSTALL_CMD += \
+            cp -rf $(LOCAL_PATH)/prebuilt/arm64/bin $(TARGET_RECOVERY_ROOT_OUT)/system/;
+    else
+        LOCAL_POST_INSTALL_CMD += \
+            cp -rf $(LOCAL_PATH)/prebuilt/arm/bin $(TARGET_RECOVERY_ROOT_OUT)/system/;
+    endif
     include $(BUILD_PHONY_PACKAGE)
 
     include $(CLEAR_VARS)
-
     LOCAL_MODULE := python3_lib
     LOCAL_MODULE_TAGS := optional
     LOCAL_MODULE_CLASS := ETC
@@ -38,11 +44,18 @@ ifneq ($(TW_EXCLUDE_PYTHON),true)
 
     LOCAL_POST_INSTALL_CMD += \
         mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/system/etc/python; \
-        cp -rf $(LOCAL_PATH)/prebuilt/lib $(TARGET_RECOVERY_ROOT_OUT)/system/etc/python/;
+        cp -rf $(LOCAL_PATH)/prebuilt/common/lib $(TARGET_RECOVERY_ROOT_OUT)/system/etc/python/;
+
+    ifeq ($(TARGET_ARCH),arm64)
+        LOCAL_POST_INSTALL_CMD += \
+            cp -rf $(LOCAL_PATH)/prebuilt/arm64/lib $(TARGET_RECOVERY_ROOT_OUT)/system/etc/python/;
+    else
+        LOCAL_POST_INSTALL_CMD += \
+            cp -rf $(LOCAL_PATH)/prebuilt/arm/lib $(TARGET_RECOVERY_ROOT_OUT)/system/etc/python/;
+    endif
     include $(BUILD_PHONY_PACKAGE)
 
     include $(CLEAR_VARS)
-
     LOCAL_MODULE := python3_scripts
     LOCAL_MODULE_TAGS := optional
     LOCAL_MODULE_CLASS := ETC
