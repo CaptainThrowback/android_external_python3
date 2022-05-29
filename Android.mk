f354c1f20ec9e8a44353acdf0f2d9151dfdeb860
Captain Throwback <captainthrowback@hotmail.com>
2022-05-29 19:28:50 -0400

    Move all files to a single package

--- Android.mk
+++ Android.mk
@@ -18,50 +18,25 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(TW_EXCLUDE_PYTHON),true)
     # Dummy file to copy prebuilt python files
     include $(CLEAR_VARS)
-    LOCAL_MODULE := python3_bin
-    LOCAL_MODULE_TAGS := optional
-    LOCAL_MODULE_CLASS := EXECUTABLES
-    LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
-
-    LOCAL_POST_INSTALL_CMD += \
-        mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/system/bin; \
-        cp -rf $(LOCAL_PATH)/prebuilt/common/bin $(TARGET_RECOVERY_ROOT_OUT)/system/;
-
-    ifeq ($(TARGET_ARCH),arm64)
-        LOCAL_POST_INSTALL_CMD += \
-            cp -rf $(LOCAL_PATH)/prebuilt/arm64/bin $(TARGET_RECOVERY_ROOT_OUT)/system/;
-    else
-        LOCAL_POST_INSTALL_CMD += \
-            cp -rf $(LOCAL_PATH)/prebuilt/arm/bin $(TARGET_RECOVERY_ROOT_OUT)/system/;
-    endif
-    include $(BUILD_PHONY_PACKAGE)
-
-    include $(CLEAR_VARS)
-    LOCAL_MODULE := python3_lib
+    LOCAL_MODULE := python3_twrp
     LOCAL_MODULE_TAGS := optional
     LOCAL_MODULE_CLASS := ETC
     LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 
     LOCAL_POST_INSTALL_CMD += \
         mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/system/etc/python; \
-        cp -rf $(LOCAL_PATH)/prebuilt/common/lib $(TARGET_RECOVERY_ROOT_OUT)/system/etc/python/;
+        cp -rf $(LOCAL_PATH)/prebuilt/common/bin $(TARGET_RECOVERY_ROOT_OUT)/system/; \
+        cp -rf $(LOCAL_PATH)/prebuilt/common/lib $(TARGET_RECOVERY_ROOT_OUT)/system/etc/python/; \
+        cp -rf $(LOCAL_PATH)/scripts $(TARGET_RECOVERY_ROOT_OUT)/system/etc/python/;
 
     ifeq ($(TARGET_ARCH),arm64)
         LOCAL_POST_INSTALL_CMD += \
+            cp -rf $(LOCAL_PATH)/prebuilt/arm64/bin $(TARGET_RECOVERY_ROOT_OUT)/system/; \
             cp -rf $(LOCAL_PATH)/prebuilt/arm64/lib $(TARGET_RECOVERY_ROOT_OUT)/system/etc/python/;
     else
         LOCAL_POST_INSTALL_CMD += \
+            cp -rf $(LOCAL_PATH)/prebuilt/arm/bin $(TARGET_RECOVERY_ROOT_OUT)/system/; \
             cp -rf $(LOCAL_PATH)/prebuilt/arm/lib $(TARGET_RECOVERY_ROOT_OUT)/system/etc/python/;
     endif
-    include $(BUILD_PHONY_PACKAGE)
-
-    include $(CLEAR_VARS)
-    LOCAL_MODULE := python3_scripts
-    LOCAL_MODULE_TAGS := optional
-    LOCAL_MODULE_CLASS := ETC
-    LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
-
-    LOCAL_POST_INSTALL_CMD += \
-        cp -rf $(LOCAL_PATH)/scripts $(TARGET_RECOVERY_ROOT_OUT)/system/etc/python/;
     include $(BUILD_PHONY_PACKAGE)
 endif
